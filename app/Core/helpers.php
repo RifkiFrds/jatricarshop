b11c90380e52f93fbee6e6108f6f9fd0f5cfa596
rifkifrds <muhamadrifkifirdaus22@gmail.com>
2026-07-03 17:14:46 +0700

    fix http

--- app/Core/helpers.php
+++ app/Core/helpers.php
@@ -14,7 +14,10 @@ if (!function_exists('view')) {
 
 if (!function_exists('base_url')) {
     function base_url($path = '') {
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+        $isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || 
+                   (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||
+                   (isset($_SERVER['HTTP_X_FORWARDED_PORT']) && $_SERVER['HTTP_X_FORWARDED_PORT'] === '443');
+        $protocol = $isHttps ? "https" : "http";
         $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
         
         $scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
